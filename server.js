763c0cfca0ba2358ed0dfd22ec3ced90a81e6a3e
Julissa Cotillo <julissacotillo@gmail.com>
2017-01-18 12:43:30 -0500

    heroku testing

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
 // Get the packages we need
 var express = require('express');
 var mongoose = require('mongoose');
-var mongodb = require("mongodb");
 var bodyParser = require('body-parser');
 var Anime = require('./app/models/anime');
 // connect to the anime mongoDB
-// mongoose.connect(process.env.MONGOLAB_URI);
-
+mongoose.connect(process.env.MONGOLAB_URI);
+mongodb://api:test@ds013584.mlab.com:13584/animes
 // Create our Express application
 var app = express();
 
 // Use the body-parser package in our application
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-var router = express.Router();
-
-// Create a new route with the prefix /animes
-var AnimesRoute = router.route('/animes/:anime_id');
 
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
 
-var db;
+// Create our Express router
+var router = express.Router();
 
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGOLAB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on anime!' });
 });
 
-// Create our Express router
+// Create a new route with the prefix /animes
+var AnimesRoute = router.route('/animes');
+
+// Create endpoint /api/animes for GET
+AnimesRoute.get(function(req, res) {
+  // Use the Anime model to find all anime
+  Anime.find(function(err, animes) {
+    if (err)
+    res.send(err);
+
+    res.json(animes);
+  });
+});
 
 // Create endpoint /api/animes for POSTS
 AnimesRoute.post(function(req, res) {
@@ -63,16 +60,9 @@ AnimesRoute.post(function(req, res) {
   });
 });
 
-// Create endpoint /api/animes for GET
-AnimesRoute.get(function(req, res) {
-  // Use the Beer model to find all beer
-  Anime.find(function(err, animes) {
-    if (err)
-      res.send(err);
 
-    res.json(animes);
-  });
-});
+// Create a new route with the /anime/:anime_id prefix
+var AnimesRoute = router.route('/animes/:anime_id');
 
 // Create endpoint /api/animes/:anime_id for PUT
 AnimesRoute.put(function(req, res) {
@@ -105,11 +95,10 @@ AnimesRoute.delete(function(req, res) {
     res.json({ message: 'Anime removed' });
   });
 });
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on anime!' });
-});
 
 // Register all our routes with /api
 app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Insert anime on port ' + port);
